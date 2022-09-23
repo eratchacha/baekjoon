9563ebabceec79be215114a10dbc67697cc4ba88
Cha Seung Hoon <58865827+eratchacha@users.noreply.github.com>
2022-09-23 14:20:03 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    for(int i = 1 ; i < n + 1 ; i++) {
+        if(i %  2 == 1)
+            answer += "수";
+       	else
+            answer += "박";
+    }
+    return answer;
+}
