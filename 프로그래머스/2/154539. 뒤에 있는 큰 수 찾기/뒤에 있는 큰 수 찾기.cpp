a1dcc52d0dfbd32d54afeea0b51f9228fb94cc9f
Cha Seung Hoon <58865827+eratchacha@users.noreply.github.com>
2024-03-21 04:54:27 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 324.98 ms, Memory: 240 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
@@ -4,24 +4,6 @@ using namespace std;
 
 vector<int> solution(vector<int> numbers) {
     vector<int> answer(numbers.size(), -1);
-//     int max = 0;
-//     int biggest = 1000000;
-    
-//     for (int i = 0 ; i < numbers.size() - 1 ; i++) {
-//         if (numbers[i] >= biggest)
-//             continue;
-        
-//         for (int j = i + 1 ; j < numbers.size() ; j++) {
-//             if (numbers[i] < numbers[j]) {
-//                 answer[i] = numbers[j];
-//                 break;
-//             }
-//         }
-        
-//         if (answer[i] == -1 && biggest > numbers[i]) {
-//             biggest = numbers[i];
-//         }
-//     }
     
     for (int i = numbers.size() - 2 ; i >= 0 ; i--) {
         for (int j = i + 1 ; j < numbers.size() ; j++) {
