fe3f516c22d05e4d9e09513ccbc335fcdd4f5a27
Cha Seung Hoon <58865827+eratchacha@users.noreply.github.com>
2024-04-19 10:49:55 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.05 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.swift
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.swift
@@ -6,8 +6,6 @@ func solution(_ num_str:String) -> Int {
     for c in num_str {
         if let num = Int(String(c)) {
             answer += num
-        } else {
-            return 1
         }
     }
     
