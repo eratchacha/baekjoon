1206f839c73d34d80191775ba67f3092dc9f47a8
Cha Seung Hoon <58865827+eratchacha@users.noreply.github.com>
2024-04-23 03:41:14 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ numbers:[Int], _ n:Int) -> Int {
+    var answer = 0
+    
+    for i in numbers {
+        answer += i
+        
+        if answer > n {
+            return answer
+        }
+    }
+    
+    return answer
+}
