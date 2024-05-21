7af085d3dabc1fdc70036546eee30a3737ad2566
Cha Seung Hoon <58865827+eratchacha@users.noreply.github.com>
2024-05-21 13:01:34 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.01 ms, Memory: 16.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ arr:[Int], _ idx:Int) -> Int {
+    
+    for i in idx ..< arr.count {
+        if arr[i] == 1 {
+            return i
+        }
+    }
+    
+    return -1
+}
