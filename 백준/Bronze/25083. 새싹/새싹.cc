aa9aa8a2d7cb3cdc7a6357bf7484288ef858b179
Cha Seung Hoon <58865827+eratchacha@users.noreply.github.com>
2024-05-24 00:00:13 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  Algorithm_C++
+//
+//  Created by 차차 on 3/7/24.
+//
+
+#include <iostream>
+#include <string>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "         ,r\'\"7"  << endl;
+    cout << "r`-_   ,'  ,/ " << endl;
+    cout << " \\. \". L_r'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+}
